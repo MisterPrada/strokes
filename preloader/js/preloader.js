aa0c65121edc61b1a55bfa61ee551bfa07bec289
MisterPrada <lesha.skorpion@gmail.com>
2020-08-01 21:39:16 +0300

    v1.5.1 - Доработан прелоадер

--- preloader/js/preloader.js
+++ preloader/js/preloader.js
@@ -1,31 +1,68 @@
-let finishPaint = 0;
-let startPaint = -198;
-let speedPaint = 0.61;
-let xPosition = "x";
-const intervalBetweenFrames = 10;
-let speedUp = 5;
-let timeToFinishPaint = 500;
+let time = 0;
+let timeBetweenLetters = 800;
 
-// Fill logo
-let paintPreloader = setInterval(function () {
-  maskRect.setAttribute(xPosition, startPaint);
-  startPaint = startPaint + speedPaint;
+let timer = setInterval(() => {
+  time = time + 1;
+}, timeBetweenLetters);
 
-  if (startPaint > finishPaint) {
-    clearInterval(paintPreloader);
+let paintSymbal = setInterval(() => {
+  switch (time) {
+    case 1:
+      primer11.setAttribute("fill", "#BA2425");
+      primer12.setAttribute("fill", "#BA2425");
+      primer13.setAttribute("fill", "#BA2425");
+      break;
+
+    case 2:
+      primer21.setAttribute("fill", "#BA2425");
+      primer22.setAttribute("fill", "#BA2425");
+      primer23.setAttribute("fill", "#BA2425");
+      break;
+
+    case 3:
+      primer31.setAttribute("fill", "#BA2425");
+      primer32.setAttribute("fill", "#BA2425");
+      primer33.setAttribute("fill", "#BA2425");
+      primer34.setAttribute("fill", "#BA2425");
+      break;
+
+    case 4:
+      primer41.setAttribute("fill", "#BA2425");
+      primer42.setAttribute("fill", "#BA2425");
+      primer43.setAttribute("fill", "#BA2425");
+      primer44.setAttribute("fill", "#BA2425");
+      primer45.setAttribute("fill", "#BA2425");
+      primer46.setAttribute("fill", "#BA2425");
+      primer47.setAttribute("fill", "#BA2425");
+      primer48.setAttribute("fill", "#BA2425");
+      primer49.setAttribute("fill", "#BA2425");
+      primer410.setAttribute("fill", "#BA2425");
+      primer411.setAttribute("fill", "#BA2425");
+      break;
+
+    case 5:
+      primer51.setAttribute("fill", "#BA2425");
+      primer52.setAttribute("fill", "#BA2425");
+      primer53.setAttribute("fill", "#BA2425");
+      primer54.setAttribute("fill", "#BA2425");
+      primer55.setAttribute("fill", "#BA2425");
+      break;
+
+    case 6:
+      primer61.setAttribute("fill", "#BA2425");
+      primer62.setAttribute("fill", "#BA2425");
+      primer63.setAttribute("fill", "#BA2425");
+      break;
+
+    case 7:
+      primer71.setAttribute("fill", "#BA2425");
+      primer72.setAttribute("fill", "#BA2425");
+      primer73.setAttribute("fill", "#BA2425");
+      primer74.setAttribute("fill", "#BA2425");
+      break;
+
+    case 8:
+      document.getElementById("preloader").style.display = "none";
+      break;
   }
-}, intervalBetweenFrames);
-
-// Tracking loading and finishing filling
-let finishPaintPreloader = setInterval(function () {
-  if (document.readyState === "complete") {
-    speedPaint = speedPaint + speedUp;
-    clearInterval(finishPaintPreloader);
-  }
-}, intervalBetweenFrames);
-
-window.onload = function () {
-  setTimeout(function () {
-    document.getElementById("preloader").style.display = "none";
-  }, timeToFinishPaint);
-};
+}, 100);
