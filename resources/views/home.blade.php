4f21fb3f3cfbd73b030955dfb89b606bf983d39f
MisterPrada <lesha.skorpion@gmail.com>
2022-01-25 18:38:50 +0300

    v1.45.1 - Добавлен Facebook Meta Pixel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,12 +15,36 @@
     <meta property="og:image" content="/img/og.png" />
     <meta property="og:description" content="{{ __('seo_description') }}" />
 
+    <meta name="facebook-domain-verification" content="ior0i28cce6wszl8wb020josuk54ez" />
+
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/locomotive-scroll.min.css">
     <link rel="stylesheet" href="css/strokes.css">
     <link rel="stylesheet" href="css/title.css">
     <link rel="stylesheet" href="css/app.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '2408149725982716');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1"
+             width="1"
+             style="display:none"
+             src="https://www.facebook.com/tr?id=2408149725982716&ev=PageView&noscript=1"
+        />
+    </noscript>
+    <!-- End Meta Pixel Code -->
 </head>
 <body data-scroll-container style="overflow: hidden;">
 
