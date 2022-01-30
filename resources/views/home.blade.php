8f7f79193ba638827249b33dddf64d4b2509bdc0
MisterPrada <lesha.skorpion@gmail.com>
2022-01-30 20:26:49 +0300

    v1.47.0 - Добавлена Яндекс Метрика

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,6 +45,21 @@
         />
     </noscript>
     <!-- End Meta Pixel Code -->
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(87335579, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/87335579" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 </head>
 <body data-scroll-container style="overflow: hidden;">
 
